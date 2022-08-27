e77f4355959d8bf5cd4304b4ba42fa0f537b94b8
hillaryomondi <hillary.okoth@strathmore.edu>
2022-08-27 12:10:29 +0300

    initial commit

---
+++ py_client/basic.py
@@ -0,0 +1,15 @@
+import requests
+
+#endpoint = "https://httpbin.org/status/200/"
+endpoint = "http://127.0.0.1:8000/api"
+
+get_response = requests.get(endpoint, json={"query": "Hello World"}) #HTTP Request
+print(get_response.text) # print raw text response
+print(get_response.status_code)
+
+# HTTP Request -> HTML
+# REST API HTTP Request -> JSON
+# JavaScript Object Notation ~ Python dict
+
+print(get_response.json()['message'])
+#print(get_response.status_code)
