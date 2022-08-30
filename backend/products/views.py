ac8f2e012a4aaa3d4f9e5b64889527912a518127
hillaryomondi <hillary.okoth@strathmore.edu>
2022-08-30 22:02:00 +0300

    Added create and list api views

--- backend/products/views.py
+++ backend/products/views.py
@@ -1,3 +1,26 @@
-from django.shortcuts import render
+from rest_framework import generics
+from .models import Product
+from .serializers import ProductSerializer
 
-# Create your views here.
+class ProductListCreateAPIView(generics.CreateListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+
+    def perform_create(self, serializer):
+        # serializer.save(user=self.request.user)
+        #print(serializer.validated_data)
+        title = serializer.validated_data.get('title')
+        content = serializer.validated_data.get('content')
+        #or None
+        if content is None:
+            content = title
+        serializer.save(content=content)
+
+product_list_create_view = ProductListCreateAPIView.as_view()
+
+class ProductListAPIView(generics.ListAPIView):
+    queryset = Product.objects.all()
+    serializer_class = ProductSerializer
+    # lookup_field = 'pk'
+
+product_list_view = ProductListAPIView.as_view()
